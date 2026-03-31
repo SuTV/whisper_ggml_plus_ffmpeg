699918797f06b7400041681edbcc9f0cab42f77b
SuTV <sutv.nd@gmail.com>
2026-03-31 22:01:14 +0700

    Update FFmpeg dependency to the latest version and adjust imports

--- lib/whisper_ggml_plus_ffmpeg.dart
+++ lib/whisper_ggml_plus_ffmpeg.dart
@@ -1,6 +1,6 @@
-import 'package:ffmpeg_kit_flutter_new_min/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter_new_min/ffmpeg_session.dart';
-import 'package:ffmpeg_kit_flutter_new_min/return_code.dart';
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_session.dart';
+import 'package:ffmpeg_kit_flutter_new/return_code.dart';
 import 'package:flutter/foundation.dart';
 import 'package:universal_io/io.dart';
 import 'package:whisper_ggml_plus/whisper_ggml_plus.dart';
